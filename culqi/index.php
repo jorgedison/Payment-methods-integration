23c074ddcbe5358ed80d0af4445514347991e593
Jorge Rodriguez Flores <jrodriguezf@outlook.com>
2023-12-13 10:24:14 -0500

    Update index.php

--- culqi/index.php
+++ culqi/index.php
@@ -2,22 +2,32 @@
 require 'vendor/autoload.php';
 
 // Configura tu API Key y tu Código de Comercio
-$SECRET_API_KEY = "sk_test_xxxxxxxxxxxxxxxxxxxx";
-$culqi = new Culqi\Culqi(array('api_key' => $SECRET_API_KEY));
+// Es recomendable guardar la clave API en un archivo de configuración o variable de entorno
+$SECRET_API_KEY = getenv('CULQI_API_KEY');
+
+// Verifica si la clave API está configurada
+if (!$SECRET_API_KEY) {
+    die("La clave API de Culqi no está configurada.");
+}
+
+$culqi = new Culqi\Culqi(['api_key' => $SECRET_API_KEY]);
 
 // Crea un cargo a una tarjeta
 try {
-  $charge = $culqi->Charges->create(
-      array(
-          "amount" => 1000,
-          "currency_code" => "PEN",
-          "email" => "test@culqi.com",
-          "source_id" => "xxx-xxxxxxxxxxxxx-xxxx-xx"
-      )
-  );
-  // Respuesta del cargo
-  print_r($charge);
+    $charge = $culqi->Charges->create([
+        "amount" => 1000,
+        "currency_code" => "PEN",
+        "email" => "test@culqi.com",
+        "source_id" => "xxx-xxxxxxxxxxxxx-xxxx-xx"
+    ]);
+
+    // Respuesta del cargo
+    echo "<pre>";
+    print_r($charge);
+    echo "</pre>";
 } catch (Exception $e) {
-  echo $e->getMessage();
+    // Maneja el error de manera más efectiva
+    error_log("Error al crear el cargo: " . $e->getMessage());
+    echo "Ocurrió un error al realizar el cargo. Por favor, inténtelo de nuevo.";
 }
 ?>
